a9ee865aafd3334200eeb8470afb38de33d5c42d
Mauricio Vasquez B <mauricio.vasquez@polito.it>
2017-04-18 11:34:33 -0500

    add support for shared tables
    Currently tables can be created in two modes:
    1. private to the ebpf program
    2. shared among all the ebpf programs created by the same user-space
    process
    
    This commit extends bcc allowing to create a table that is shared among
    a set of specific ebpf programs.
    This is useful when creating network functions that are composed
    by more than 1 ebpf program, a map is shared among all the programs of
    the network function but isolated from different instances of that
    function.
    
    Signed-off-by: Mauricio Vasquez B <mauricio.vasquez@polito.it>

--- src/cc/frontends/clang/b_frontend_action.cc
+++ src/cc/frontends/clang/b_frontend_action.cc
@@ -1090,6 +1090,7 @@ bool BTypeVisitor::VisitVarDecl(VarDecl *Decl) {
     TableStorage::iterator table_it;
     table.name = Decl->getName();
     Path local_path({fe_.id(), table.name});
+    Path maps_ns_path({fe_.maps_ns(), table.name});
     Path global_path({table.name});
     QualType key_type, leaf_type;
 
@@ -1175,9 +1176,11 @@ bool BTypeVisitor::VisitVarDecl(VarDecl *Decl) {
     } else if (A->getName() == "maps/cpumap") {
       map_type = BPF_MAP_TYPE_CPUMAP;
     } else if (A->getName() == "maps/extern") {
-      if (!fe_.table_storage().Find(global_path, table_it)) {
-        error(Decl->getLocStart(), "reference to undefined table");
-        return false;
+      if (!fe_.table_storage().Find(maps_ns_path, table_it)) {
+        if (!fe_.table_storage().Find(global_path, table_it)) {
+          error(Decl->getLocStart(), "reference to undefined table");
+          return false;
+        }
       }
       table = table_it->second.dup();
       table.is_extern = true;
@@ -1191,6 +1194,17 @@ bool BTypeVisitor::VisitVarDecl(VarDecl *Decl) {
         return false;
       }
       fe_.table_storage().Insert(global_path, table_it->second.dup());
+      return true;
+    } else if(A->getName() == "maps/shared") {
+      if (table.name.substr(0, 2) == "__")
+        table.name = table.name.substr(2);
+      Path local_path({fe_.id(), table.name});
+      Path maps_ns_path({fe_.maps_ns(), table.name});
+      if (!fe_.table_storage().Find(local_path, table_it)) {
+        error(Decl->getLocStart(), "reference to undefined table");
+        return false;
+      }
+      fe_.table_storage().Insert(maps_ns_path, table_it->second.dup());
       return true;
     }
 
@@ -1321,11 +1335,13 @@ void BTypeConsumer::HandleTranslationUnit(ASTContext &Context) {
 BFrontendAction::BFrontendAction(llvm::raw_ostream &os, unsigned flags,
                                  TableStorage &ts, const std::string &id,
                                  const std::string &main_path,
-                                 FuncSource &func_src, std::string &mod_src)
+                                 FuncSource &func_src, std::string &mod_src,
+                                 const std::string &maps_ns)
     : os_(os),
       flags_(flags),
       ts_(ts),
       id_(id),
+      maps_ns_(maps_ns),
       rewriter_(new Rewriter),
       main_path_(main_path),
       func_src_(func_src),
